7cc1ea17d1c2f68720728ac936cfefcfe1564a25
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-04-07 12:46:12 +1200

    Create spinner container

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -18,8 +18,24 @@ function Register() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
-  const {user, isLoading, isError, isSuccess, message} = useSelector(
-    (state) => state.auth)
+  const {user, isLoading, isError, isSuccess, message} = useSelector
+  (
+    (state) => state.auth
+  )
+
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+
+    if(isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
